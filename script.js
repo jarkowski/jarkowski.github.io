a1f690c5671e8877a837d86a48e1fac0e1539188
David Jarkowski <4302314+jarkowski@users.noreply.github.com>
2024-02-27 14:01:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Let's write a JavaScript function that takes the JSON structure and dynamically generates the HTML
-
-// Since the actual crafting of the code does not require execution, we'll prepare the function as a string.
 // This function will:
 // - Parse the provided JSON structure.
 // - Recursively build the HTML structure.
@@ -12,20 +9,24 @@ function loadFolderStructure() {
         function createFolderHTML(folder) {
             var subfoldersHTML = '';
             if (folder.subfolders && folder.subfolders.length > 0) {
-                subfoldersHTML = '<ul>' + folder.subfolders.map(createFolderHTML).join('') + '</ul>';
+                // Map through each subfolder and recursively call createFolderHTML
+                subfoldersHTML = '<ul style="display: none;">' + folder.subfolders.map(createFolderHTML).join('') + '</ul>';
             }
-            return '<li><a href="javascript:void(0);">' + '<div class="member-viewbox">' + '<div class="member-image">' + '<img src="Folder_Icon.svg" alt="Member"></img>' + '<div class="member-details">' + folder.name + '</div>' + '</div>' + '</div>' + '</a>' + '  ' + subfoldersHTML + '</li>';
+            return '<li><a href="javascript:void(0);">' + '<div class="member-viewbox">' + '<div class="member-image">' + '<img src="Folder_Icon.svg" alt="Folder Icon"></img>' + '<div class="member-details">' + folder.name + '</div>' + '</div>' + '</div>' + '</a>' + subfoldersHTML + '</li>';
         }
 
         var htmlStructure = '<ul>' + createFolderHTML(folderStructure) + '</ul>';
-        $('#dynamic-folder-structure').html(htmlStructure);
+        $('#dynamic-folder-structure').html(htmlStructure).find('> ul').show(); // Show only the first level
 
         // Re-attach the click event logic for the dynamically created elements
-        $('.oid-tree li').off('click').on('click', function (e) {
-            var children = $(this).find('> ul');
-            if (children.is(":visible")) children.hide('fast').removeClass('active');
-            else children.show('fast').addClass('active');
-            e.stopPropagation();
+        $('#dynamic-folder-structure li > a').off('click').on('click', function (e) {
+            var children = $(this).parent().find('> ul');
+            if (children.is(":visible")) {
+                children.hide('fast');
+            } else {
+                children.show('fast');
+            }
+            e.stopPropagation(); // Prevent the event from bubbling up to parent elements
         });
     });
 }
